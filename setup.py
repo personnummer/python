f05049bc540aad59d802bbac5f8f8355f9d168e1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-17 11:02:47 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='personnummer',
-      version='3.0.0',
+      version='3.0.1',
       description='Validate Swedish social security numbers',
       url='http://github.com/personnummer/python',
       author='Personnummer and Contributors',
