4bffa2747b89630194a972762d7b5885b46433e7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-10-28 16:39:56 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-      name='personnummer',
-      version='3.0.2',
-      description='Validate Swedish personal identity numbers',
-      url='http://github.com/personnummer/python',
-      author='Personnummer and Contributors',
-      author_email='hello@personnummer.dev',
-      license='MIT',
-      packages=['personnummer'],
-      test_suite='nose.collector',
-      tests_require=['nose', 'mock'],
+    name='personnummer',
+    version='3.0.3',
+    description='Validate Swedish personal identity numbers',
+    url='http://github.com/personnummer/python',
+    author='Personnummer and Contributors',
+    author_email='hello@personnummer.dev',
+    license='MIT',
+    packages=['personnummer'],
+    test_suite='nose.collector',
+    tests_require=['nose', 'mock'],
 )
