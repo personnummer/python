c906d54bd43a700fc8b493463192e434ec056891
Danny Hajj <dannyhajj@users.noreply.github.com>
2023-11-24 11:04:17 +0100

    Migrate from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='personnummer',
-    version='3.0.6',
-    description='Validate Swedish personal identity numbers',
-    url='http://github.com/personnummer/python',
-    author='Personnummer and Contributors',
-    author_email='hello@personnummer.dev',
-    license='MIT',
-    packages=['personnummer'],
-    test_suite='pytest',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': ['personnummer = personnummer.main:main']
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
